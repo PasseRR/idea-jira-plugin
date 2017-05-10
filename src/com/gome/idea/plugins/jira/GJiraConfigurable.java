c0c4ed8c74ca8b6ef6d63c48442b025e4f0ca449
PasseRR <xie__hai@sina.com>
2017-05-10 14:47:21 +0800

    settings界面

---
+++ src/com/gome/idea/plugins/jira/GJiraConfigurable.java
@@ -0,0 +1,69 @@
+package com.gome.idea.plugins.jira;
+
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * @author xiehai1
+ * @date 2017/05/10 10:49
+ * @Copyright(c) gome inc Gome Co.,LTD
+ */
+public class GJiraConfigurable implements SearchableConfigurable {
+    public GJiraConfigurable() {
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return this.getId();
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return this.getId();
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        return GJiraForm.me().getRootComponent();
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void disposeUIResources() {
+
+    }
+
+    @NotNull
+    @Override
+    public String getId() {
+        return "Gjira";
+    }
+
+    @Nullable
+    @Override
+    public Runnable enableSearch(String s) {
+        return null;
+    }
+}
---
+++ src/com/gome/idea/plugins/jira/GJiraUi.java
@@ -0,0 +1,12 @@
+package com.gome.idea.plugins.jira;
+
+import javax.swing.*;
+
+/**
+ * @author xiehai1
+ * @date 2017/05/10 14:06
+ * @Copyright(c) gome inc Gome Co.,LTD
+ */
+public interface GJiraUi {
+    JComponent getRootComponent();
+}
